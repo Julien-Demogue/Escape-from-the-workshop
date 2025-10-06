4b24cac5c8b53594857441d2f5b32c0acb1ab921
eric.girard <delasalle.sio.girard.e@gmail.com>
2025-10-06 15:04:26 +0200

    ajout du script de base de donnees

---
+++ bdd.sql
@@ -0,0 +1,75 @@
+CREATE DATABASE if not exists escape_game;
+USE escape_game;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE
+
+);
+CREATE TABLE IF NOT EXISTS infos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+   description TEXT NOT NULL
+
+);
+
+CREATE TABLE IF NOT EXISTS challenges (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    infoId INT NOT NULL,
+    flag VARCHAR(100) NOT NULL,
+    reward INT NOT NULL,
+    FOREIGN KEY (infoId) REFERENCES infos(id)
+
+);
+
+CREATE TABLE if not exists illustrations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    infosId INT NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    FOREIGN KEY (infosId) REFERENCES infos(id)
+
+);
+
+CREATE table if not EXISTS party (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    adminUserId INT NOT NULL,
+    endTime TIMESTAMP NULL,
+    FOREIGN KEY (adminUserId) REFERENCES users(id)
+);
+
+CREATE TABLE if not EXISTS groupe (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    partyId INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    code VARCHAR(50) NOT NULL UNIQUE,
+    score INT DEFAULT 0,
+    FOREIGN KEY (partyId) REFERENCES party(id)
+);
+
+CREATE TABLE if not EXISTS groupe_user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    groupId INT NOT NULL,
+    userId INT NOT NULL,
+    FOREIGN KEY (groupId) REFERENCES groupe(id),
+    FOREIGN KEY (userId) REFERENCES users(id)
+);
+
+CREATE TABLE if not EXISTS messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    groupId INT NOT NULL,
+    senderId INT NOT NULL,
+    content TEXT NOT NULL,
+    sendDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (groupId) REFERENCES groupe(id),
+    FOREIGN KEY (senderId) REFERENCES users(id)
+);
+
+CREATE TABLE if NOT EXISTS challenge_progress (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    groupId INT NOT NULL,
+    challengeId INT NOT NULL,
+    isCompleted BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (groupId) REFERENCES groupe(id),
+    FOREIGN KEY (challengeId) REFERENCES challenges(id)
+);
