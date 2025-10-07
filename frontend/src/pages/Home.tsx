4ff804b0b28c46f9785020cb774299ca89acb02f
LucasOnandi <lucasonandi93@gmail.com>
2025-10-07 10:31:58 +0200

    puzzle

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,6 +6,7 @@ const Home = () => {
       <h1>Accueil</h1>
       <p>Bienvenue sur la page d'accueil.</p>
       <Link to="/about" className="red">Aller à la page About</Link>
+      <Link to="/puzzle" className="red">Aller à la page Puzzle</Link>
     </div>
   )
 }
